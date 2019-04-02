dac705a4b54a50d6b59303401a1620722566f834
Savchenkov Pavel <35948980+p-savchenkov@users.noreply.github.com>
2019-04-02 16:19:10 +0300

    Test1 (#7)
    * add test with params@
    
    * test1
    
    * test2 (#8)
    
    * test2
    
    * test3 (#9)

---
+++ src/test/java/junitTestWithParams.java
@@ -0,0 +1,61 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import  static org.assertj.core.api.Assertions.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+
+@RunWith(Parameterized.class)
+public class junitTestWithParams {
+
+  @BeforeClass
+  public static void setUpUrl(){
+
+    RestAssured.baseURI = "http://autoru-api-server-int.vrts-slb.test.vertis.yandex.net/";
+    RestAssured.basePath = "1.0/";
+
+  }
+
+  @Parameters(name = "{index}: user {0} - response {1}")
+  public static Iterable<Object[]> data(){
+    return Arrays.asList(new Object[][]{
+            {"20621551", 200},
+            {"123", 404}
+    });
+  }
+
+  private String uId;
+  private int responseCode;
+
+  public junitTestWithParams(String uId, int responseCode){
+    this.uId = uId;
+    this.responseCode = responseCode;
+
+  }
+
+  @Test
+  public void userTest(){
+    Response resp =
+            given().
+                    header("X-Authorization", "Vertis swagger").
+                    header("X-uid", this.uId).
+                    log().all().
+            when().
+                    get("user").
+            then().
+                    contentType(ContentType.JSON).
+                    extract().response();
+
+    assertThat(resp.getStatusCode()).isEqualTo(this.responseCode);
+
+
+  }
+}
---
+++ src/test/java/test1.java
@@ -0,0 +1,4 @@
+public class test1git status {
+  test2
+  test3
+}
